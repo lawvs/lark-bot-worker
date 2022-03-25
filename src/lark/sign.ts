07547c599600ac8a37dd561e8993c860b7957d6e
lawvs <18554747+lawvs@users.noreply.github.com>
2022-03-25 18:39:36 +0800

    fix: web crypto sign

--- src/lark/sign.ts
+++ src/lark/sign.ts
@@ -1,3 +1,29 @@
+/**
+ * Ported from [How to get HMAC with Crypto Web API](https://stackoverflow.com/questions/47329132/how-to-get-hmac-with-crypto-web-api)
+ */
+const webCryptoHmacSha256 = async (secret: string, body = "") => {
+  const enc = new TextEncoder();
+  const algorithm = { name: "HMAC", hash: "SHA-256" };
+
+  const key = await crypto.subtle.importKey(
+    "raw",
+    enc.encode(secret),
+    algorithm,
+    false,
+    ["sign"]
+  );
+  const signature = await crypto.subtle.sign(
+    algorithm.name,
+    key,
+    enc.encode(body)
+  );
+  const digest = btoa(String.fromCharCode(...new Uint8Array(signature)));
+  return digest;
+};
+
+/**
+ * See [Security settings - Feishu](https://www.feishu.cn/hc/en-US/articles/360024984973#lineguid-n2ogCT)
+ */
 export const larkSign = async (
   secret: string,
   timestamp = String(Date.now()).slice(0, 10)
@@ -5,12 +31,8 @@ export const larkSign = async (
   const payload = `${timestamp}\n${secret}`;
 
   if (process.env.CF_WORKER) {
-    // Cloudflare Worker
-    const signBuffer = await crypto.subtle.digest(
-      "SHA-256",
-      new TextEncoder().encode(payload)
-    );
-    const sign = btoa(String.fromCharCode(...new Uint8Array(signBuffer)));
+    // Cloudflare Workers
+    const sign = await webCryptoHmacSha256(payload);
     return {
       sign,
       timestamp,
